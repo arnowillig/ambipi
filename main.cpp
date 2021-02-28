59e9b9447a54fbbf7261e9329c4e9bc93ddd67bc
Arno Willig <arno.willig@hertz.st>
2021-02-28 23:25:24 +0100

    Tabs

--- main.cpp
+++ main.cpp
@@ -154,7 +154,7 @@ int main(int argc, char *argv[])
 	ambiPi.setColor(0,0,0);
 	ambiPi.render();
 #ifndef _DEVEL_
-		fb.clear();
+	fb.clear();
 #endif
 	fprintf(stderr, "Stopping AmbiPi done..\n");
 	return 0;
@@ -164,36 +164,36 @@ int main(int argc, char *argv[])
 
 int dispmanx(void)
 {
-    bcm_host_init();
+	bcm_host_init();
 
-    DISPMANX_DISPLAY_HANDLE_T display = vc_dispmanx_display_open(0);
+	DISPMANX_DISPLAY_HANDLE_T display = vc_dispmanx_display_open(0);
 
-    DISPMANX_MODEINFO_T info;
-    int ret = vc_dispmanx_display_get_info(display, &info);
-    assert(ret == 0);
-    printf("Display is %d x %d\n", info.width, info.height );
+	DISPMANX_MODEINFO_T info;
+	int ret = vc_dispmanx_display_get_info(display, &info);
+	assert(ret == 0);
+	printf("Display is %d x %d\n", info.width, info.height );
 
-    void* image = calloc( 1, info.width * 3 * info.height);
-    
-    uint32_t vc_image_ptr;
-    DISPMANX_RESOURCE_HANDLE_T resource = vc_dispmanx_resource_create(VC_IMAGE_RGB888, info.width, info.height, &vc_image_ptr);
+	void* image = calloc( 1, info.width * 3 * info.height);
 
-    VC_RECT_T rect;
-    DISPMANX_TRANSFORM_T transform;
-    vc_dispmanx_snapshot(display, resource, transform);
-    vc_dispmanx_rect_set(&rect, 0, 0, info.width, info.height);
-    vc_dispmanx_resource_read_data(resource, &rect, image, info.width*3); 
+	uint32_t vc_image_ptr;
+	DISPMANX_RESOURCE_HANDLE_T resource = vc_dispmanx_resource_create(VC_IMAGE_RGB888, info.width, info.height, &vc_image_ptr);
 
-    FILE *fp = fopen("out.ppm", "wb");
-    fprintf(fp, "P6\n%d %d\n255\n", info.width, info.height);
-    fwrite(image, info.width*3*info.height, 1, fp);
-    fclose(fp);
+	VC_RECT_T rect;
+	DISPMANX_TRANSFORM_T transform;
+	vc_dispmanx_snapshot(display, resource, transform);
+	vc_dispmanx_rect_set(&rect, 0, 0, info.width, info.height);
+	vc_dispmanx_resource_read_data(resource, &rect, image, info.width*3);
 
-    ret = vc_dispmanx_resource_delete(resource);
-    assert( ret == 0 );
-    ret = vc_dispmanx_display_close(display);
-    assert( ret == 0 );
+	FILE *fp = fopen("out.ppm", "wb");
+	fprintf(fp, "P6\n%d %d\n255\n", info.width, info.height);
+	fwrite(image, info.width*3*info.height, 1, fp);
+	fclose(fp);
 
-    return 0;
+	ret = vc_dispmanx_resource_delete(resource);
+	assert( ret == 0 );
+	ret = vc_dispmanx_display_close(display);
+	assert( ret == 0 );
+
+	return 0;
 }
 #endif
